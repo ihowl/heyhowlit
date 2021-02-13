208ae6c1caaca471d7c1dabfa0b2cea7e7d6e8ce
Ian Howlett <ian.howlett@gmail.com>
2021-02-13 10:05:49 -0500

    adding header

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'heyhowlit!', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'This is a landing page. But from where are you landing?', // e.g: Welcome to my website
 };
 
 // HERO DATA
